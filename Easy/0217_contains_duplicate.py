722458a01f43a38841df5428e648d54114209a08
exarion <exarionbase@gmail.com>
2026-06-14 10:26:41 +0300

    Update 0217_contains_duplicate.py
    Add solution for LeetCode 217 Contains Duplicate in Python

--- Easy/0217_contains_duplicate.py
+++ Easy/0217_contains_duplicate.py
@@ -1,1 +1,8 @@
+# Problem Link: https://leetcode.com/problems/contains-duplicate
+# Difficulty: Easy
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(nums) != len(set(nums))
