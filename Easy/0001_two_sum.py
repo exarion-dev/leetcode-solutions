70346d6e8cd287101a8dd642355961f17182aaa1
exarion-dev <exarionbase@gmail.com>
2026-06-15 10:10:20 +0300

    Add solution for LeetCode 1 Two Sum in Python

---
+++ Easy/0001_two_sum.py
@@ -0,0 +1,14 @@
+# Problem Link: https://leetcode.com/problems/two-sum
+# Difficulty: Easy
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [i, seen[complement]]
+            seen[num] = i
+        return []
